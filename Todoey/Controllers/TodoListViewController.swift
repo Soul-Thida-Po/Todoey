b6cb4f039ac8bdeaf20f8dbaa5842ae9d1081b69
Soulthidapo <soethidapo@gmail.com>
2019-08-22 13:13:17 +0900

    Install RealmSwift Pods.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
+    //, UISearchBarDelegate, UIPickerViewDelegate, UIImagePickerControllerDelegate
     
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")    //I have mistake documentationDirectory
+    var selectedCategory : Category?{
+        didSet{
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     //let defaults = UserDefaults.standard                // #Singleton
     
@@ -20,17 +28,9 @@ class TodoListViewController: UITableViewController {
         
         super.viewDidLoad()
         
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        loadItems()
-        
-        
-        //To show append list
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//
-//            itemArray = items
-//        }
-        
+        //loadItems()
     }
 
     //MARK: Tableview Datasource Methods Step 1
@@ -47,17 +47,8 @@ class TodoListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
-        //Ternary Operator ==>
-        //value = condition ? valueIfTrue : valueIfFalse
-        
         cell.accessoryType = item.done == true ? .checkmark : .none
-        
-//        if item.done == true{ //itemArray[indexPath.row]
-//            cell.accessoryType = .checkmark
-//        }else{
-//            cell.accessoryType = .none
-//        }
-        
+ 
         return cell
     }
     
@@ -67,12 +58,12 @@ class TodoListViewController: UITableViewController {
         
         //checkmark
         
-        //print(itemArray[indexPath.row])
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done  //New Add *****
         
         saveItems()
-        //tableView.reloadData() ////new add
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -89,9 +80,11 @@ class TodoListViewController: UITableViewController {
             //What the happen once the user clicks the add item button on our UIAlert
             print("Success!!!!!!!!!!!")
             
-            let newItem = Item()
-            newItem.title = textField.text!
+        //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
             
+        let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             //self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
@@ -101,7 +94,7 @@ class TodoListViewController: UITableViewController {
         alert.addTextField { (alertTextField) in
             
             alertTextField.placeholder = "Create new item"
-            print(alertTextField.text)
+            print(alertTextField.text!)
             textField = alertTextField
         }
         
@@ -110,29 +103,66 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do{
-            let data =  try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            
+            try context.save()
         }catch{
-            print("Error encoding item array, \(error)")
+            print("Error saving context, \(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadItems(){
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
         
-        if let data = try? Data(contentsOf: dataFilePath!){
+        if let addtionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+        }else{
+            request.predicate = categoryPredicate
+        }
+        
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,predicate])
+//
+//        request.predicate = compoundPredicate
+        
+        do{
+            itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK: - Search bar methods
+
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        //request
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        print(searchBar.text!)
+        
+        //filter
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+    
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
             
-            let decoder = PropertyListDecoder()
-            do{
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch{
-                print("Error decode item , \(error)")
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
-            
-            
         }
     }
     
